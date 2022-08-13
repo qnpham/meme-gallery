2964cd08f48f133ee1c5b01b8cce20ada3172871
Quan Pham <mockanterr@gmail.com>
2022-08-13 21:04:47 +0000

    completed meme gallery project

---
+++ main.js
@@ -0,0 +1,26 @@
+const $gallery = document.querySelector('main');
+const imageUrls = [
+  'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+  'https://pbs.twimg.com/media/FaDpx5jXgAEKeft?format=jpg&name=medium',
+  'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'
+];
+const $imageUrlInput = document.querySelector('input');
+const $addImageButton = document.querySelector('button');
+$addImageButton.addEventListener('click', function () {
+  if ($imageUrlInput.value !== '') {
+    imageUrls.push($imageUrlInput.value);
+    $imageUrlInput.value = '';
+    updateGallery();
+  }
+});
+
+function updateGallery() {
+  $gallery.innerHTML = '';
+  for (let i = 0; i < imageUrls.length; i++) {
+    const $imageElement = document.createElement('img');
+    $imageElement.className = 'gallery-image';
+    $imageElement.src = imageUrls[i];
+    $gallery.appendChild($imageElement);
+  }
+}
+updateGallery();
